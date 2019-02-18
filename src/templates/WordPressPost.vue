89debc285d83ffa6dc4eb9278d77a14e4c3a337c
unknown <gabriele@surface.intra.adsol.ch>
2019-02-18 15:30:04 +0100

    adj to tracking

--- src/templates/WordPressPost.vue
+++ src/templates/WordPressPost.vue
@@ -39,18 +39,27 @@ query Post ($path: String!) {
 export default {
   metaInfo () { return {
     title: this.$page.wordPressPost.title,
-    script: [{
-      key: 'owaTrackerCode',
-      innerHTML: `//<![CDATA[
-        var owa_baseUrl = 'https://wombats.ch/owa/';
-        var owa_cmds = owa_cmds || [];
-        owa_cmds.push(['setSiteId', '7c4439b1f96509791a4d3967c4a64176']);
-        owa_cmds.push(['trackPageView']);
-        owa_cmds.push(['trackClicks']);
-        owa_cmds.push(['setPageType','post']);
-        //]]>`,
-      type:"text/javascript"
-    }]
-  }}
+    meta: [
+      {     
+        key: 'owaTrackerCode',
+        innerHTML: `//<![CDATA[
+          var owa_baseUrl = 'https://wombats.ch/owa/';
+          var owa_cmds = owa_cmds || [];
+          owa_cmds.push(['setSiteId', '37d007a56d816107ce5b52c10342db37']);
+          owa_cmds.push(['setPageType','post'])
+          owa_cmds.push(['trackPageView']);
+          owa_cmds.push(['trackClicks']);
+          
+          (function() {
+            var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+            owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+            _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+            var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+          }());
+          //]]>`,
+        type:"text/javascript" }
+    ]
+    }
+  }
 }
 </script>
--- src/templates/WordPressTribeEvents.vue
+++ src/templates/WordPressTribeEvents.vue
@@ -53,21 +53,30 @@ query Event ($path: String!) {
 export default {
   data: function() { return {} },
   metaInfo() {
-    return{
+    return {
       title: this.$page.wordPressTribeEvents.title,
-      script: [{
-        key: 'owaTrackerCode',
-        innerHTML: `//<![CDATA[
-          var owa_baseUrl = 'https://wombats.ch/owa/';
-          var owa_cmds = owa_cmds || [];
-          owa_cmds.push(['setSiteId', '7c4439b1f96509791a4d3967c4a64176']);
-          owa_cmds.push(['trackPageView']);
-          owa_cmds.push(['trackClicks']);
-          owa_cmds.push(['setPageType','event']);
-          //]]>`,
-        type:"text/javascript"
-      }]
-  }},
+      meta: [
+        {     
+          key: 'owaTrackerCode',
+          innerHTML: `//<![CDATA[
+            var owa_baseUrl = 'https://wombats.ch/owa/';
+            var owa_cmds = owa_cmds || [];
+            owa_cmds.push(['setSiteId', '37d007a56d816107ce5b52c10342db37']);
+            owa_cmds.push(['setPageType','event'])
+            owa_cmds.push(['trackPageView']);
+            owa_cmds.push(['trackClicks']);
+            
+            (function() {
+              var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+              owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+              _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+              var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+            }());
+            //]]>`,
+          type:"text/javascript" }
+      ]
+    }
+  },
   computed: {
     event: function() {
       let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
