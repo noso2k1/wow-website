b1a64acd4bda12fd16cf94d247f8aa8dc11f1103
unknown <gabriele@surface.intra.adsol.ch>
2019-02-19 17:33:25 +0100

    owa code to prod

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -194,7 +194,7 @@ export default {
         innerHTML: `//<![CDATA[
           var owa_baseUrl = 'https://wombats.ch/owa/';
           var owa_cmds = owa_cmds || [];
-          owa_cmds.push(['setSiteId', '37d007a56d816107ce5b52c10342db37']);
+          owa_cmds.push(['setSiteId', '7c4439b1f96509791a4d3967c4a64176']);
           owa_cmds.push(['setPageType','home'])
           owa_cmds.push(['trackPageView']);
           owa_cmds.push(['trackClicks']);
